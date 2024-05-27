9acb31c3b222af1ee4fe40aa5dbf93fb7e2a9ba5
Evelyn-DeLima <fabycomy@hotmail.com>
2024-05-27 15:19:36 +0200

    Added moment and time zone code in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A [</small>]"
+  );
+
+  let barcelonaElement = document.querySelector("#barcelona");
+  let barcelonaDateElement = barcelonaElement.querySelector(".date");
+  let barcelonaTimeElement = barcelonaElement.querySelector(".time");
+  let barcelonaTime = moment().tz("Europe/Barcelona");
+
+  barcelonaDateElement.innerHTML = barcelonaTime.format("MMMM Do YYYY");
+  barcelonaTimeElement.innerHTML = barcelonaTime.format(
+    "h:mm:ss [<small>]A [</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
